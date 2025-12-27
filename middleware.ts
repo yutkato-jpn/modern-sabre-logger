a0132b21ed8240fc63b3d6c038a36adc70cdd60d
Yutaro KATO <yutkatoleaf@gmail.com>
2025-12-28 02:43:56 +0900

    Fix middleware type error

--- middleware.ts
+++ middleware.ts
@@ -14,7 +14,7 @@ export async function middleware(request: NextRequest) {
         getAll() {
           return request.cookies.getAll()
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: any) {
           cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
           supabaseResponse = NextResponse.next({
             request,
