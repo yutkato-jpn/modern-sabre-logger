7e7de8b9ef8ba2e0dead3a72e08286118c51c7bb
Yutaro KATO <yutkatoleaf@gmail.com>
2025-12-28 02:47:18 +0900

    Fix middleware loop type error

--- middleware.ts
+++ middleware.ts
@@ -15,12 +15,12 @@ export async function middleware(request: NextRequest) {
           return request.cookies.getAll()
         },
         setAll(cookiesToSet: any) {
-          cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
+          cookiesToSet.forEach((cookie: any) => request.cookies.set(cookie.name, cookie.value))
           supabaseResponse = NextResponse.next({
             request,
           })
-          cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options)
+          cookiesToSet.forEach((cookie: any) =>
+            supabaseResponse.cookies.set(cookie.name, cookie.value, cookie.options)
           )
         },
       },
